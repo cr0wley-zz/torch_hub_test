3d04f7c9140a7129a572e8c8b3e7a6b228d242fe
cr0wley-zz <dev.onga@gmail.com>
2021-09-23 01:18:31 +0530

    hubcomf changes

--- hubconf.py
+++ hubconf.py
@@ -10,9 +10,9 @@ MODEL_PATH = os.path.join("output", "model_wt.pth")
 def custom_model():
     # initialize model instance
     # load weights from path
-    mlp = mlp.get_training_model()
-    mlp.load_state_dict(torch.load(MODEL_PATH))
-    return mlp
+    model = mlp.get_training_model()
+    model.load_state_dict(torch.load(MODEL_PATH))
+    return model
 
 
 
